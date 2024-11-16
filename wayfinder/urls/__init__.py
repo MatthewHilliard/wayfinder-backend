a79659e39e7c4d040c191074d4ad5d309e48e6a4
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-11-15 23:58:35 -0500

    Initial commit

---
+++ wayfinder/urls/__init__.py
@@ -0,0 +1,10 @@
+# urls/__init__.py
+# from django.contrib import admin
+from django.urls import include, path
+
+# In order to have seperate url files, django requires this file and to import the urls here:
+
+# All URL routes
+urlpatterns = [
+
+]
