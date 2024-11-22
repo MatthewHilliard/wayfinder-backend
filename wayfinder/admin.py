d0d7347a5bc5376a2c4c21c66607ac4c0d582c5e
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-11-21 19:23:52 -0500

    Registered models for admin

--- wayfinder/admin.py
+++ wayfinder/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Location, User
+from .models import Location, User, Experience, Tip, Wishlist, WishlistItem, Rating, Tag
 
 admin.site.register(Location)
 admin.site.register(User)
+admin.site.register(Experience)
+admin.site.register(Tip)
+admin.site.register(Wishlist)
+admin.site.register(WishlistItem)
+admin.site.register(Rating)
+admin.site.register(Tag)
