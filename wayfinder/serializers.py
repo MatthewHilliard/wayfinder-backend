bc91ef569197ac08f20a35b9cf18d52f310215e7
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-11-22 11:22:33 -0500

    Serialize tags for get requests

--- wayfinder/serializers.py
+++ wayfinder/serializers.py
@@ -4,8 +4,15 @@ from .models import *
 # NOTE: Each model should have a corresponding serializer to handle validation and
 # conversion of incoming data, as well as serializing outgoing data to be
 # returned in responses.
-
+        
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+        
 class ExperienceSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)  # Serialize related tags for GET requests
+    
     class Meta:
         model = Experience
         fields = '__all__'
