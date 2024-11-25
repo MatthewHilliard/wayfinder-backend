a768ab89265335332f79ca0467f023be92a7818b
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-11-25 01:04:51 -0500

    Added filter by tags

--- wayfinder/urls/experience_urls.py
+++ wayfinder/urls/experience_urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     
     # GET Requests
     path('get_experiences/', experience_views.get_experiences, name='get_experiences'),
+    path('get_experiences_with_filters/', experience_views.get_experiences_with_filters, name='get_experiences_with_filters'),
     path('get_experience_by_id/<str:experience_id>/', experience_views.get_experience_by_id, name='get_experience_by_id'),
 ]
--- wayfinder/views/experience_views.py
+++ wayfinder/views/experience_views.py
@@ -19,7 +19,41 @@ def get_experiences(request):
     Returns:
         JsonResponse: JSON response with all experiences
     '''
+    experiences = Experience.objects.all().order_by('-average_rating', '-number_of_ratings')
+    serializer = ExperienceSerializer(experiences, many=True)
+    return JsonResponse({'data': serializer.data})
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def get_experiences_with_filters(request):
+    '''
+    Get all experiences from the database with filters.
+
+    Parameters:
+        request: Request object with a query parameter "tags" (comma-separated list of tag names)
+
+    Returns:
+        JsonResponse: JSON response with all experiences
+    '''
+    # Step 1: Filter based on tags
+    # Get tags from query parameters
+    tags = request.GET.get('tags', None)
+    
+    # Start with all experiences
     experiences = Experience.objects.all()
+    
+    if tags:
+        # Convert comma-separated string into a list
+        tag_list = tags.split(',')
+        
+        # Filter experiences by tags
+        experiences = experiences.filter(tags__name__in=tag_list).distinct()
+        
+    # Sort by average rating and number of ratings
+    experiences = experiences.order_by('-average_rating', '-number_of_ratings')
+    
+    # Serialize and return the response
     serializer = ExperienceSerializer(experiences, many=True)
     return JsonResponse({'data': serializer.data})
 
