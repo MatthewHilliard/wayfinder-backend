bbf85c0a49491a3407b97765df776679e28c226a
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-12-06 23:23:31 -0500

    Changes

--- app/settings.py
+++ app/settings.py
@@ -195,15 +195,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Media files (uploaded by users)
-if os.getenv('ENV') == 'PRODUCTION':
-    # Production settings
-    MEDIA_URL = 'wayfinder-backend-prod-b2b08ed79f38.herokuapp.com/media/'
-    MEDIA_ROOT = BASE_DIR / 'media'
-else:
-    # Local development settings
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
